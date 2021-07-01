45c197404fc300d76e78b7f78b2160495d17e0fd
florence1920 <jetaime12121@gmail.com>
2021-07-01 17:12:42 +0900

    related Posts 틀

---
+++ front/src/components/RPosts.vue
@@ -0,0 +1,37 @@
+<template>
+    <ul class=rPostList>
+        <li>
+            <p class="title">카카오 AI추천 : 토픽 모델링과 MAB를 이용한 카카오 개인화 추천</p>
+            <p class="date">2021.06.05</p>
+            <p class="cont"> 글 작성에는 추천팀 sasha.k와 marv.20가 함께해 주셨습니다. 머신러닝에 대한 기초 지식이 있고, 추천 알고리즘에 관심이 있는 분들에게 카카오 추천팀이 개인화 추천 기술을 활용하는 방법에 대해 공유합니다. 우리의 일상 속에는 수많은 추천이 함께합니다. 카카오 추천팀은 카카오가 제공하는 서비스들에서 여러분들이 더 좋은 추천을 경험하실 수 있도록 매일 노력하고 있는데요, 오늘은 수없이 많은 정보 가운데 여러분 한 분 […]</p>
+            <div class="imgBG">
+                <img src="@/assets/images/rpImg.png">
+            </div>
+        </li>
+
+        <li>
+            <p class="title">카카오 AI추천 : 토픽 모델링과 MAB를 이용한 카카오 개인화 추천</p>
+            <p class="date">2021.06.05</p>
+            <p class="cont"> 글 작성에는 추천팀 sasha.k와 marv.20가 함께해 주셨습니다. 머신러닝에 대한 기초 지식이 있고, 추천 알고리즘에 관심이 있는 분들에게 카카오 추천팀이 개인화 추천 기술을 활용하는 방법에 대해 공유합니다. 우리의 일상 속에는 수많은 추천이 함께합니다. 카카오 추천팀은 카카오가 제공하는 서비스들에서 여러분들이 더 좋은 추천을 경험하실 수 있도록 매일 노력하고 있는데요, 오늘은 수없이 많은 정보 가운데 여러분 한 분 […]</p>
+            <div class="imgBG">
+                <img src="@/assets/images/rpImg.png">
+            </div>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+    .rPostList {padding: 0 294px 0 0;}
+    .rPostList li {width:866px; margin: 0 0 16px; padding: 35px 32px 31px; background:#fff;}
+    .rPostList li .title {font-weight: 600; font-size: 20px; color: #111; letter-spacing: -1.5px; }
+    .rPostList li .date {margin: 10px 0 0;font-size: 14px; color: #aaa;}
+    .rPostList li .cont { display: -webkit-box; letter-spacing: -2px; overflow: hidden; margin-top: 15px; font-size: 16px; line-height: 26px; max-height: 78px; -webkit-line-clamp: 3; -webkit-box-orient: vertical; text-overflow: ellipsis;}
+    .rPostList li .imgBG {background:#000; margin:30px 0 0;}
+    .rPostList li .imgBG img {display:block; width: 100%; opacity: 0.9;}
+</style>
---
+++ front/src/components/Top.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="top">
+        <div class="w1160">
+            <div class="titWrap">
+                <p class="tit">Blog</p>
+                <p class="sub">카카오의 서비스를 만드는 개발자들의 실제 개발 사례를 중심으로 기술과 노하우를 생생하게 전달합니다.</p>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style scoped>
+    .top {background-image:url(../assets/images/intro_blog.png);}
+    .top .w1160 {width:1160px; height: 474px; margin:0 auto; padding:220px 0 0;}
+    .top .w1160 .titWrap {color:#fff;}
+    .top .w1160 .titWrap .tit {font-size:56px; line-height: 67px;}
+    .top .w1160 .titWrap .sub {font-size:18px; margin:11px 0 0; opacity:0.7; letter-spacing: -0.05em;}
+</style>
+
